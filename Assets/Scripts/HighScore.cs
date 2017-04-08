1e78f08b63a6d93fb7f239bff4626ad46c4494e9
Kalisz <akalisz@bgh-edelstahl.com>
2017-04-09 00:22:17 +0200

    WIP Main menu with background and audio options, high score table, volume options in game

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -1,10 +1,89 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
+[Serializable]
 public class HighScore : MonoBehaviour {
 
     private int score = 0;
+    private string currentPlayer;
+    private float totalGameTime = 0;
+
+    private List<ScoreItem> table;
+
+    private void Start()
+    {
+        loadScore();
+    }
+
+    private void Update()
+    {
+        totalGameTime = Time.time;
+    }
+
+    public void addScoreItem()
+    {
+        // Add Score to List
+        ScoreItem item = new ScoreItem();
+        // Must be set from elsewhere before saving
+        item.name = this.currentPlayer;
+        item.score = this.score;
+        item.time = this.totalGameTime;
+
+        table.Add(item);
+    }
+
+    public void saveScore()
+    {
+
+        if (table.Count > 1)
+        {
+            // Sortiere nach score, sortiere nach time wenn Übereinstimmung von score
+            // Lösche überflüssige Elemente
+            table.Sort();
+            if (table.Count > 10)
+            {
+                table = table.GetRange(0, 10);
+            }
+            table.Sort((x, y) => x.time.CompareTo(y.time));
+        }
+
+        for (int i = 0; i < table.Count; i++)
+        {
+            
+            string jsonData = table[i].getJSON();
+            print(jsonData);
+
+            try
+            {
+                PlayerPrefs.SetString("highscore" + i, jsonData);
+                PlayerPrefs.Save();
+            }
+            catch (System.Exception err)
+            {
+                Debug.Log("Error: " + err);
+            }
+        }
+    }
+
+    public void loadScore(){
+
+        for (int i = 0; i < 10; i++)
+        {
+            string jsonData = PlayerPrefs.GetString("highscore" + i);
+            table.Add(ScoreItem.createFromJSON(jsonData));
+
+            print(jsonData);
+        }
+    }
+
+    public void deleteScores()
+    {
+        // Löscht alle einträge in Einstellungen (nicht nur HighScores)
+        PlayerPrefs.DeleteAll();
+    }
 
     public int getScore()
     {
@@ -15,5 +94,11 @@ public class HighScore : MonoBehaviour {
     {
         this.score++;
     }
+
+    public float getGameTime()
+    {
+        return this.totalGameTime;
+    }
+
 
 }
---
+++ Assets/Scripts/ScoreItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class ScoreItem : IComparable<ScoreItem> {
+
+    public int score;
+    public float time;
+    public string name;
+
+    public static ScoreItem createFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<ScoreItem>(jsonString);
+    }
+
+    public string getJSON()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public int CompareTo(ScoreItem item)
+    {
+        return score.CompareTo(item);
+    }
+
+    //public static Comparison<ScoreItem> Comparison = delegate (ScoreItem x, ScoreItem y)
+    //{
+    //    return x.time.CompareTo(y.time);
+    //};
+}
